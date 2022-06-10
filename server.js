c950a5383c7a1dd4cf6d694ecc1bc4a860bf6e48
Corey Levine <coreylevine2000@gmail.com>
2022-06-09 20:51:14 -0400

    adam and I altering the server.js file to work with handlebars and const for socket.io

--- server.js
+++ server.js
@@ -1,41 +1,65 @@
-const path = require('path');
-const express = require('express');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({
+layoutsDir: __dirname + '/views',
+}));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 3001;
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
-app.use(routes);
+const rooms = { }
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+
+app.get('/', (req, res) => {
+    res.render('landpage', { rooms: rooms })
+  })
+  
+  app.post('/room', (req, res) => {
+    if (rooms[req.body.room] != null) {
+      return res.redirect('/')
+    }
+    rooms[req.body.room] = { users: {} }
+    res.redirect(req.body.room)
+    // Send message that new room was created
+    io.emit('room-created', req.body.room)
+  })
+  
+  app.get('/:room', (req, res) => {
+    if (rooms[req.params.room] == null) {
+      return res.redirect('/')
+    }
+    res.render('room', { roomName: req.params.room })
+  })
+  
+  server.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+    
+
+  io.on('connection', socket => {
+    socket.on('new-user', (room, name) => {
+      socket.join(room)
+      rooms[room].users[socket.id] = name
+      socket.to(room).broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', (room, message) => {
+      socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+      getUserRooms(socket).forEach(room => {
+        socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+        delete rooms[room].users[socket.id]
+      })
+    })
+  })
+  
+  function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+      if (room.users[socket.id] != null) names.push(name)
+      return names
+    }, [])
+  }
