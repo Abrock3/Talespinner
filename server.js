a5ddfb3f4bdde134b8187e8cf591ae2373f3c2cf
Laura Antunez <lauraantunez024@gmail.com>
2022-06-16 15:52:23 -0400

    login page front end working but needs to be hooked up routes

--- server.js
+++ server.js
@@ -65,37 +65,6 @@ app.get('/login', (req, res) => {
   res.render('login', { layout: 'main' });
 });
 
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
 // /room is not a page, it's a route for the "lobby" page to send info when a user is attempting to create a room
 app.post('/room', (req, res) => {
   // If the user types a room name that already exists, they'll be redirected back to /lobby to try again
