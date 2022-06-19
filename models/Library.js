4b4dbc2d269d53384bf2848dfc5a23953c50610e
Adam Brock <a.paulbrock@gmail.com>
2022-06-19 13:32:34 -0400

    Added server.js code to support sequelize; modified the models and seeds folders, and modified package.json to allow the user to 'npm run seed'

--- models/Library.js
+++ models/Library.js
@@ -6,36 +6,37 @@ class Library extends Model {
 }
 
 Library.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        stories: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'user',
-                key: 'id',
-            },
-        },  
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'library'
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      default: "Untitled Story"
+    },
+    story: {
+      type: DataTypes.STRING(16000),
+      allowNull: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'library',
+  }
 );
 
 module.exports = Library;
--- seeds/seed.js
+++ seeds/seed.js
@@ -2,21 +2,20 @@ const sequelize = require('../config/connection');
 const { User, Library } = require('../models');
 
 const userData = require('./userData.json');
-// const libraryData = require('./libraryData.json');
+const libraryData = require('./libraryData.json');
 
 const seedDatabase = async () => {
-  await User.sync({ force: true });
-  await Library.sync({ force: true });
+  await sequelize.sync({ force: true });
 
-  // await User.bulkCreate(userData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-  // await Library.bulkCreate(libraryData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
+  await Library.bulkCreate(libraryData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };
